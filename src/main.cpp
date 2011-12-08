30f8bb4be7d55506d83cbfec05e807a508f007c0
Christopher Ogden <sydius@gmail.com>
2011-12-07 21:05:50 -0800

    Added ticking.

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,19 @@
 #include <boost/asio.hpp>
+#include <chrono>
+#include <iostream>
 
 int main(int argc, char * argv[])
 {
     boost::asio::io_service ioService;
 
+    auto time = std::chrono::monotonic_clock::now();
     while (true) {
+        static const auto duration = std::chrono::milliseconds(50);
+        if (std::chrono::monotonic_clock::now() - time > duration) {
+            time += duration;
+
+            std::cout << "tick\n";
+        }
         ioService.poll();
     }
 
